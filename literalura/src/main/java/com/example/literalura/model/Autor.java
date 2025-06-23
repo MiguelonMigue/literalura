2058f0582448a35c6103eee0368a8202ff6f7450
miguelonmiguelon <miguelcatholic@gmail.com>
2025-06-22 23:53:42 -0500

    subiendo cambios

--- literalura/src/main/java/com/example/literalura/model/Autor.java
+++ literalura/src/main/java/com/example/literalura/model/Autor.java
@@ -1,49 +1,4 @@
 package com.example.literalura.model;
 
-import jakarta.persistence.*;
-@Entity
-@Table(name = "autores")
 public class Autor {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nombre;
-    private Integer nacimiento;
-    private Integer fallecimiento;
-
-    public Autor(DatosAutor datosAutor) {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Integer getNacimiento() {
-        return nacimiento;
-    }
-
-    public void setNacimiento(Integer nacimiento) {
-        this.nacimiento = nacimiento;
-    }
-
-    public Integer getFallecimiento() {
-        return fallecimiento;
-    }
-
-    public void setFallecimiento(Integer fallecimiento) {
-        this.fallecimiento = fallecimiento;
-    }
 }
